414228ba13deaddcfa4db8fc88bc7adadeb22ae3
raheel-muhammad <122502569+raheel-muhammad@users.noreply.github.com>
2023-01-12 11:50:14 +0500

    Create index.py

---
+++ index.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+st.set_page_config(page_title="My Webpage",page_icon=":tada:",layout="wide")  
+st.subheader("hi, i am Muhammad Raheel :wave:")  
+st.title("A JS Developer from Pakistan")  
+st.write("i am passionate about to finding ways ")   
