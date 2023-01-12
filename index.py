6013161752a069ef9531d1c92e0fbd68a6875fbc
raheel-muhammad <122502569+raheel-muhammad@users.noreply.github.com>
2023-01-12 12:15:46 +0500

    Update index.py

--- index.py
+++ index.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.set_page_config(page_title="My Webpage",page_icon=":tada:",layout="wide")  
 st.subheader("hi, i am Muhammad Raheel :wave:")  
 st.title("A JS Developer from Pakistan")  
-st.write("i am passionate about to finding ways ")   
+st.write("I am passionate about to learning new skills ")
